6b1b0ed52fbab5bbae80fc075d7e6bd084fe38ec
SHEN Lue <omit1996@gmail.com>
2019-04-05 18:39:56 +0800

    initial commit for garch pricer

---
+++ Codes/garchPricer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 17:22:55 2019
+
+@author: lueshen
+"""
+
+
+
+def garchPricer(garchModel, ulyTS, strike, expiry):
+    
+    return "April Fool!"
+    
+
+fairPrice = garchPricer("garchModel", "ulyTS", "strike", "expiry")
+
+print(fairPrice)
