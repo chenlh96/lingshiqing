586aa7f0e4b86af3f5b9516f4df7a507dbe6e6b8
clhtony <clhengtony@163.com>
2019-05-09 16:11:25 +0800

    bug fixed, add simulation

--- Codes/binomialTreePricer.py
+++ Codes/binomialTreePricer.py
@@ -1,38 +1,36 @@
 import numpy as np
 
-class averagePriceBinomialTree:
+class asianOptionBinomialTree:
 
-    def __init__(self, num_steps, volatility, time_period, oneOverRho, intereat_rage):
+    def __init__(self, num_steps, volatility, time_period, oneOverRho, interest_rate):
         self.num_steps = num_steps
         self.volatility = volatility
         self.time_period = time_period
         self.oneOverRho = oneOverRho
-        self.discount_factor = np.exp(-1 * intereat_rage * self.time_period)
+        self.discount_factor = np.exp(-1 * interest_rate * self.time_period)
+        self.half_len_grid = self.num_steps * self.oneOverRho
 
-        self.averagePriceTree = np.zeros((self.num_steps + 1, 2 * self.num_steps * oneOverRho))
+        self.averagePriceTree = np.zeros(2 * self.num_steps * oneOverRho + 1)
         self.assetPriceTree = np.zeros((self.num_steps + 1, self.num_steps + 1))
-        self.optionPriceTree = np.zeros((self.num_steps + 1, self.num_steps + 1,  2 * self.num_steps * self.oneOverRho))
+        self.optionPriceTree = np.zeros((self.num_steps + 1,  2 * self.num_steps * self.oneOverRho + 1))
 
     def forwardInduction(self):
         self.up_factor = np.exp(self.volatility * np.sqrt(self.time_period))
-        rho = 1 / self.oneOverRho
 
         for i in range(self.num_steps + 1):
             lower_bound = -i
             for j in range(i + 1):
                 self.assetPriceTree[i, j] = self.init_price * (self.up_factor ** lower_bound)
                 lower_bound += 2
-
-            len_shooting_index = min(2 ** i, i * self.oneOverRho)
-            for j in range(2 * len_shooting_index):
-                jump = j - len_shooting_index
-                self.averagePriceTree[i, j] = self.init_price * (self.up_factor ** (jump * rho))
+        
+        for j in range(2 * self.num_steps * self.oneOverRho + 1):
+            jump = j - self.num_steps * self.oneOverRho
+            self.averagePriceTree[j] = self.init_price * (self.up_factor ** (jump / self.oneOverRho))
 
         for s in range(self.num_steps + 1):
-            len_shooting_index = min(2 ** i, i * self.oneOverRho)
-            for k in range(len_shooting_index):
-                self.optionPriceTree[self.num_steps, s, k] = max(self.averagePriceTree[self.num_steps, k] - self.strike, 0)
-
+            for k in range(2 * self.num_steps * self.oneOverRho + 1):
+                self.optionPriceTree[s, k] = max(self.averagePriceTree[k] - self.strike, 0)
+                # self.optionPriceTree[s, k] = max(self.averagePriceTree[k] - self.assetPriceTree[self.num_steps - s, s], 0)
 
     def grid(self, n, k, j, plus): 
         denominator = self.volatility * np.sqrt(self.time_period) / self.oneOverRho
@@ -46,56 +44,56 @@ class averagePriceBinomialTree:
         proba_up = (1 / self.discount_factor - 1 / self.up_factor) / (self.up_factor - 1 / self.up_factor)
 
         for n in reversed(range(self.num_steps)):
-            len_shooting_index = max(1, min(2 ** n, n * self.oneOverRho))
             lower_bound = -n
-            for i in range(n + 1):
-                for k in range(2 * len_shooting_index):
-                    k_up = self.grid(n, k - len_shooting_index, lower_bound, 1) + len_shooting_index
-                    k_down = self.grid(n, k - len_shooting_index, lower_bound, -1) + len_shooting_index
+            i = np.array([j for j in range(n + 1)]) + 1
+            k_idx = [k for k in range(- n * self.oneOverRho, n * self.oneOverRho + 1)]
+            k_idx = np.array(k_idx)
+            k_up = self.grid(n, k_idx, lower_bound, 1)
+            k_down = self.grid(n, k_idx, lower_bound, -1)
 
-                    k_up_floor = max(int(np.floor(k_up)), 0)
-                    k_up_ceil = min(k_up_floor + 1, 2 * len_shooting_index - 1 + 2)
+            k_up_floor = np.maximum(np.floor(k_up + self.half_len_grid).astype(int), 0)
+            k_up_ceil = np.minimum(k_up_floor + 1, self.half_len_grid * 2)
 
-                    # average_price_up = ((n + 1) * self.averagePriceTree[n, k] + self.assetPriceTree[n + 1, i + 1]) / (n + 2)
-                    average_price_up = self.init_price * self.up_factor ** ((k_up - len_shooting_index) / self.oneOverRho)
-                    factor_interpolation_up = np.log(average_price_up / self.averagePriceTree[n + 1, k_up_floor]) / delta_y
+            # average_price_up = ((n + 1) * self.averagePriceTree[n, k] + self.assetPriceTree[n + 1, i + 1]) / (n + 2)
+            average_price_up = self.init_price * self.up_factor ** (k_up / self.oneOverRho)
+            factor_interpolation_up = np.log(average_price_up / self.averagePriceTree[k_up_floor]) / delta_y
 
-                    option_price_up = factor_interpolation_up * self.optionPriceTree[n + 1, i + 1, k_up_ceil] + \
-                        (1 - factor_interpolation_up) * self.optionPriceTree[n + 1, i + 1, k_up_floor]
-                    
-                    k_down_floor = max(int(np.floor(k_down)), 0)
-                    k_down_ceil = min(k_down_floor + 1, 2 * len_shooting_index - 1 + 2)
+            option_price_up = factor_interpolation_up * self.optionPriceTree[1:(n + 2), k_up_ceil] + \
+                (1 - factor_interpolation_up) * self.optionPriceTree[1:(n + 2), k_up_floor]
+            
+            k_down_floor = np.maximum(np.floor(k_down + self.half_len_grid).astype(int), 0)
+            k_down_ceil = np.minimum(k_down_floor + 1, self.half_len_grid * 2)
 
-                    # average_price_down = ((n + 1) * self.averagePriceTree[n, k] + self.assetPriceTree[n + 1, i - 1]) / (n + 2)
-                    average_price_down = self.init_price * self.up_factor ** ((k_down - len_shooting_index)  / self.oneOverRho)
-                    factor_interpolation_down = np.log(average_price_down / self.averagePriceTree[n + 1, k_down_floor]) / delta_y
+            # average_price_down = ((n + 1) * self.averagePriceTree[n, k] + self.assetPriceTree[n + 1, i - 1]) / (n + 2)
+            average_price_down = self.init_price * self.up_factor ** (k_down / self.oneOverRho)
+            factor_interpolation_down = np.log(average_price_down / self.averagePriceTree[k_down_floor]) / delta_y
 
-                    assert self.averagePriceTree[n + 1, k_down_floor] != 0
+            # assert self.averagePriceTree[n + 1, k_down_floor] != 0
 
-                    option_price_down = factor_interpolation_down * self.optionPriceTree[n + 1, i + 1, k_down_ceil] + \
-                        (1 - factor_interpolation_down) * self.optionPriceTree[n + 1, i + 1, k_down_floor]
+            option_price_down = factor_interpolation_down * self.optionPriceTree[0:(n + 1), k_down_ceil] + \
+                (1 - factor_interpolation_down) * self.optionPriceTree[0:(n + 1), k_down_floor]
 
-                    self.optionPriceTree[n, i, k] = proba_up * option_price_up + (1 - proba_up) * option_price_down
-                    self.optionPriceTree[n, i, k] *= self.discount_factor
-                    
-                lower_bound -= 2
+            self.optionPriceTree[0:(n + 1), k_idx + self.half_len_grid] = proba_up * option_price_up + (1 - proba_up) * option_price_down
+            self.optionPriceTree[0:(n + 1), k_idx + self.half_len_grid] *= self.discount_factor
+                
+            lower_bound -= 2
+            
 
     def getOptionPrice(self, init_price, strike):
         self.init_price = init_price
         self.strike = strike
-
         self.forwardInduction()
         self.backwardInduction()
-
-        return self.optionPriceTree[0, 0, 0]
+        return self.optionPriceTree[0, self.half_len_grid]
 
 
 def test_main():
-    test_tree = averagePriceBinomialTree(2, 0.1, 0.12, 3, 0.05)
-    option_price = test_tree.getOptionPrice(100, 90)
+    test_tree = averagePriceBinomialTree(63, 0.0075, 1 / 63, 3, 0.03)
+    option_price = test_tree.getOptionPrice(2753, 2680)
     print(option_price)
 
 
 if __name__ == "__main__":
     test_main()
+
 
